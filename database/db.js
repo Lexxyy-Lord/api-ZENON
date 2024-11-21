1d60e5cfe7f2e74df9f595c538a7dfcd521f5fd2
Lixeyy <felixmahesa11@gmail.com>
2024-11-21 21:50:04 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'mongodb+srv://apilixey:felix1123@apilixey.qmbgx.mongodb.net/';
 try {
-if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://apilixey:felix1123@apilixey.qmbgx.mongodb.net/') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
